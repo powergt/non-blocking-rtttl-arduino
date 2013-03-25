a14bd34dacc5cb7ca879fe6c00a98a5b214d897f
Cefn Hoile <cefn_github@cefn.com>
2013-03-25 01:53:56 +0000

    Fixed space in RTTTL example song

--- rtttl/examples/RamSong/RamSong.ino
+++ rtttl/examples/RamSong/RamSong.ino
@@ -1,7 +1,7 @@
 #include <rtttl.h>
 
 //note this is not declared in PROGMEM
-const char song[] = "AxelF:d=4,o=5,b=125:32p,8g,8p,16a#.,8p,16g,16p,16g,8c6, 8g,8f,8g,8p,16d.6,8p,16g,16p,16g,8d#6,8d6,8a#,8g,8d6,8g6, 16g,16f,16p,16f,8d,8a#,2g,p,SS,16f6,8d6,8c6,8a#,g,8a#.,16g, 16p,16g,8c6,8g,8f,g,8d.6,16g,16p,16g,8d#6,8d6,8a#,8g,8d6, 8g6,16g,16f,16p,16f,8d,8a#,2g";
+const char song[] = "AxelF:d=4,o=5,b=125:32p,8g,8p,16a#.,8p,16g,16p,16g,8c6,8g,8f,8g,8p,16d.6,8p,16g,16p,16g,8d#6,8d6,8a#,8g,8d6,8g6,16g,16f,16p,16f,8d,8a#,2g,p,SS,16f6,8d6,8c6,8a#,g,8a#.,16g,16p,16g,8c6,8g,8f,g,8d.6,16g,16p,16g,8d#6,8d6,8a#,8g,8d6,8g6,16g,16f,16p,16f,8d,8a#,2g";
 
 unsigned long printedTime = -1;
 unsigned long printedPeriod = 500;
